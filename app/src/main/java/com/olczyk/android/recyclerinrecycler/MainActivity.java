e480db3ca7400cc2fa3b18df82f03782b90d4367
Olczyk <30321069+tr0lczyk@users.noreply.github.com>
2019-02-18 17:24:37 +0100

    added different animations

--- app/src/main/java/com/olczyk/android/recyclerinrecycler/MainActivity.java
+++ app/src/main/java/com/olczyk/android/recyclerinrecycler/MainActivity.java
@@ -17,15 +17,33 @@ public class MainActivity extends AppCompatActivity {
     @ViewById
     RecyclerView vertical_recycler;
 
+    @ViewById
+    RecyclerView vertical_recycler2;
+
+    @ViewById
+    RecyclerView vertical_recycler3;
+
     List<VerticalObject> verticalObjects = new ArrayList<>();
+
+    List<Note> notes = new ArrayList<>();
 
     @AfterViews
     public void aVoid(){
-        createNoteList();
+        createNote2List();
         vertical_recycler.setHasFixedSize(true);
-        vertical_recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        VerticalAdapter adapter = new VerticalAdapter(verticalObjects,this);
+        vertical_recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        HorizontalAdapter adapter = new HorizontalAdapter(notes,this);
         vertical_recycler.setAdapter(adapter);
+
+        vertical_recycler2.setHasFixedSize(true);
+        vertical_recycler2.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        HorizontalAdapter adapter2 = new HorizontalAdapter(notes,this);
+        vertical_recycler2.setAdapter(adapter2);
+
+        vertical_recycler3.setHasFixedSize(true);
+        vertical_recycler3.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        HorizontalAdapter adapter3 = new HorizontalAdapter(notes,this);
+        vertical_recycler3.setAdapter(adapter3);
     }
 
     private void createNoteList(){
@@ -39,6 +57,13 @@ public class MainActivity extends AppCompatActivity {
             }
             singleObject.setNotes(notes);
             verticalObjects.add(singleObject);
+        }
+    }
+
+    private void createNote2List(){
+        for (int i = 0; i <= 20; i++){
+            Note note = new Note("dupa " + i, "button " + i);
+            notes.add(note);
         }
     }
 }
